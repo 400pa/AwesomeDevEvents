42d15afd4dcf8f131948985b9ceff5f79eef15cd
Paulo Lacerda <contatopaulolacerda@gmail.com>
2023-07-13 22:29:12 -0300

    Add project files.

---
+++ AwesomeDevEvents.API/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AwesomeDevEvents.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeDevEvents.API.Persistence
+{
+   //OBJETO QUE IRÁ ARMAZENAR O ESTADO DO NOSSO BANCO DE DADOS//
+    public class DevEventsDbContext : DbContext
+    {
+        public DevEventsDbContext(DbContextOptions<DevEventsDbContext> options) : base(options)
+        { 
+        
+        }
+        
+        public DbSet<DevEvent> DevEvents { get; set; }
+        public DbSet<DevEventSpeaker> DevEventsSpeaker { get; set; }
+        
+    }
+}
