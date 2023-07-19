8609e627da32fe3c867acb21c6ad8746d0b3941d
Paulo Lacerda <contatopaulolacerda@gmail.com>
2023-07-18 21:53:04 -0300

    Confg Ef Core

--- AwesomeDevEvents.API/Persistence/DevEventsDbContext.cs
+++ AwesomeDevEvents.API/Persistence/DevEventsDbContext.cs
@@ -13,6 +13,40 @@ namespace AwesomeDevEvents.API.Persistence
         
         public DbSet<DevEvent> DevEvents { get; set; }
         public DbSet<DevEventSpeaker> DevEventsSpeaker { get; set; }
+
+        //CONFIG DO EF CORE//
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<DevEvent>(e =>
+
+            {
+                e.HasKey(de => de.Id);
+                
+                e.Property(de => de.Title).IsRequired(false);
+
+                e.Property(de => de.Description)
+                    .HasMaxLength(200)
+                    .HasColumnType("varchar(200)");
+
+                e.Property(de => de.StartDate)
+                    .HasColumnName("Start_Date");
+
+                e.Property(de => de.EndDate)
+                    .HasColumnName("End_Date");
+
+                e.HasMany(de => de.Speakers)
+                    .WithOne()
+                    .HasForeignKey(s => s.DevEventId);
+                 
+
+            });
+
+            builder.Entity<DevEventSpeaker>(e =>
+            {
+                e.HasKey(de => de.Id);
+
+            });
+        }
         
     }
 }
